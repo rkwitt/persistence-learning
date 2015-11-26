9eec16e55789babf13769ef859f471caccc2b863
Roland Kwitt <rkwitt@gmx.at>
2015-11-26 11:05:36 +0100

    ENH: Help for pl_write_persistence_diagram.m

--- code/matlab/utilities/pl_write_persistence_diagram.m
+++ code/matlab/utilities/pl_write_persistence_diagram.m
@@ -1,5 +1,26 @@
 function pl_write_persistence_diagram(filename,type,data)
-    
+% PL_WRITE_PERSISTENCE_DIAGRAM writes points in a persistence diagram to a
+% file compatible with DIPHA or DIONYSUS.
+%
+%   PL_WRITE_PERSISTENCE_DIAGRAM(FILENAME, TYPE, DATA) writes points of a
+%   persistence diagram in the N x 3 array DATA to a file (FILENAME) that
+%   can be read by either DIPHA (when TYPE is 'dipha') or DIONYSUS (when 
+%   TYPE is 'dionysus'). 
+%
+%   The DATA array is contains in the FIRST column the dimension of the
+%   points, e.g., 0, 1, the SECOND column contains the birth times, the
+%   THIRD column contains the death times. For example,
+%
+%   data = [
+%       0 1.1 1.2, ...
+%       0 1.3 1.4];
+%   pl_write_persistence_diagram('/tmp/diagram','dipha',data); 
+%
+%   will write the file /tmp/diagram.bin in DIPHA-compatible format
+%   containing two points (1.1,1.2) and (1.3,1.4).
+%   
+% Author(s): Roland Kwitt, 2015
+
     N = size(data,1);
     if strcmp(type,'dipha')
         filename = sprintf('%s.bin',filename);
